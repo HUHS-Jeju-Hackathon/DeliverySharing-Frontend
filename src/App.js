d39c4b89ab1daaf29703dee3254d335b8a0f3f1b
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-07-16 01:36:40 +0900

    임시

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import JoinPage from './pages/JoinPage';
 import ListPage from './pages/ListPage';
+import LoginPage from './pages/LoginPage';
 import MyPage from './pages/MyPage';
 import WritingPage from './pages/WritingPage';
 import WritingViewPage from './pages/WritingViewPage';
@@ -12,6 +14,8 @@ function App() {
         <Route path="/writing" element={<WritingPage />} />
         <Route path="/writingView" element={<WritingViewPage />} />
         <Route path="/myInfo" element={<MyPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<JoinPage />} />
       </Routes>
     </BrowserRouter>
   );
