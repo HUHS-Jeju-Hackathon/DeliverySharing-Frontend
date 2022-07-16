49eff21060a3e649377d3125a2e8fd391c9fdba9
hyunjukim <khj9872100@gmail.com>
2022-07-16 12:53:23 +0900

    최종

--- src/pages/WritingViewPage.js
+++ src/pages/WritingViewPage.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const WritingViewPage = () => {
+
+  useEffect(() => {
+    window.location.href = "/ListPage.html"
+  }, [])
+
   return <>개별 글 확인 페이지</>;
 };
 
